a84c5d5b634f7e085e53e135f429a51c6082cb31
Jake Song <mubok0212@gmail.com>
2026-05-24 00:27:28 +0900

    chore: add some debug codes

--- open-env/echo.py
+++ open-env/echo.py
@@ -1,23 +1,34 @@
 import asyncio
 from echo_env import CallToolAction, EchoEnv
 
+url = "https://openenv-echo-env.hf.space"
+
 async def main():
     # Connect to a running Space (async context manager)
-    async with EchoEnv(base_url="http://localhost:8001") as client:
+    async with EchoEnv(base_url=url) as client:
         # Reset the environment
         result = await client.reset()
-        print("before: ", result)
-        print("before: ", result.observation)  # "Echo environment ready!"
+        print("result: ", result)
+        print("obs.result: ", result.observation)  # "Echo environment ready!"
+        
+        tools = await client.list_tools()
+        print([t.name for t in tools])
 
         # Send messages
         result = await client.step(
             CallToolAction(
                 tool_name="echo_message",
-                arguments={"message": "Hello, World!"},
+                arguments={"message": "Hello World!"},
             )
         )
-        print("after: ", result)
-        print("after: ", result.observation.result)  # "Hello, World!"
-        print("after: ", result.reward)
-
+        result = await client.step(
+            CallToolAction(
+                tool_name="echo_with_length",
+                arguments={"message": "Hello World!"},
+            )
+        )
+        print("result: ", result)
+        print("obs.result: ", result.observation.result)  # "Hello"
+        print("reward: ", result.reward)
+       
 asyncio.run(main())
--- open-env/run_wordle.py
+++ open-env/run_wordle.py
@@ -1,14 +1,16 @@
+import asyncio
+
 from textarena_env import TextArenaEnv, TextArenaAction
 
-textarena_url = "https://Jakemu-wordle.hf.space" # Duplicate the Space and update this!
-# local_url = "http://localhost:8000"
+url = "https://jakemu-textarena.hf.space" # Duplicate the Space and update this!
+# url = "http://localhost:8000"
 
 class WordleEnv:
     def __init__(self):
-        self.client = TextArenaEnv(base_url=textarena_url).sync()
+        self.client = TextArenaEnv(base_url=url)
 
-    def reset(self, **kwargs) -> None | str:
-        result = self.client.reset()
+    async def reset(self, **kwargs) -> None | str:
+        result = await self.client.reset()
         # The game returns cumulative feedback each turn (new text appended at the end), so
         # we store the previous full response and slice out only the newly appended part.
         self._last_full_feedback = result.observation.messages[0].content
@@ -16,7 +18,7 @@ class WordleEnv:
         self.done = False
         return self._last_full_feedback
 
-    def guess(self, guess: str) -> str:
+    async def guess(self, guess: str) -> str:
         """
         Make a guess in the Wordle environment.
 
@@ -28,7 +30,7 @@ class WordleEnv:
         """
         if self.done:
             raise ValueError("Game over.")
-        result = self.client.step(TextArenaAction(message=guess))
+        result = await self.client.step(TextArenaAction(message=guess))
         _full_feedback = result.observation.messages[0].content
         # Just take the new feedback since the last guess
         feedback = _full_feedback[len(self._last_full_feedback):]
@@ -41,11 +43,14 @@ class WordleEnv:
         self.done = result.done
         return feedback
 
-env = WordleEnv()
-result = env.reset()
-print(result)
+async def main():
+    env = WordleEnv()
+    result = await env.reset()
+    print(result)
 
-guesses = ["Frame", "ghjslo", "Crane", "Sloth", "Picky", "Bound", "Grown"]
-for guess in guesses:
-    feedback = env.guess(guess)
-    print(f"Guess: {guess}, Feedback: {feedback}, Reward: {env.reward}, Done: {env.done}")
+    guesses = ["Frame", "ghjslo", "Crane", "Sloth", "Picky", "Bound", "Grown"]
+    for guess in guesses:
+        feedback = await env.guess(guess)
+        print(f"Guess: {guess}, Feedback: {feedback}, Reward: {env.reward}, Done: {env.done}")
+
+asyncio.run(main())
