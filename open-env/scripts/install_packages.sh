3a3daa16d027b5b4595bfa62d83713543c13ab1e
Jake Song <mubok0212@gmail.com>
2026-06-02 16:57:33 +0900

    chore

--- open-env/scripts/install_packages.sh
+++ open-env/scripts/install_packages.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 uvx hf auth login
 uv venv
-uv pip install "trl[vllm]"
+uv pip install -e ../trl[vllm]
 uv pip install -U transformers
-uv pip install wandb bitsandbytes kernels
+uv pip install wandb bitsandbytes kernels==0.14.0
 uv pip install -e ../OpenEnv/envs/agent_world_model_env
 uv pip install python-dotenv
