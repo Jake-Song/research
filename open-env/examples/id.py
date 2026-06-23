fdb44151ff9e752b818df9ba9ef1c46fb9398575
Jake Song <mubok0212@gmail.com>
2026-06-23 14:13:30 +0900

    clean up

--- open-env/examples/id.py
+++
@@ -1,5 +0,0 @@
-k = "hello"
-print(id(k))
-del k
-k = "hello"
-print(id(k))
--- open-env/examples/terminus.py
+++
@@ -1,9 +0,0 @@
-from terminus_env import TerminusEnv
-
-with TerminusEnv(base_url="http://localhost:8000").sync() as env:
-    env.reset(
-        setup=["mkdir -p /home/user/work"],
-        verify=["test -f /home/user/work/answer.txt"],
-    )
-    print(env.call_tool("terminal", command="echo done > /home/user/work/answer.txt"))
-    print(env.call_tool("terminal", final_answer="done"))
